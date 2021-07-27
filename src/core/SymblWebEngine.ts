e025053c0840415cd8b1e76fa342cfc7cc52b26c
Adam Voliva <adamvoliva@gmail.com>
2021-07-27 13:26:26 -0700

    Changes to code.

--- src/core/SymblWebEngine.ts
+++ src/core/SymblWebEngine.ts
@@ -3,20 +3,20 @@ const sdk = require('../../scripts/client.sdk.min.js');
 export = class SymblWebEngine {
     logMonitor: typeof sdk.logger = sdk.logger;
     errorHandler: typeof sdk.errorHandler = sdk.errorHandler;
-    sdk: typeof sdk = sdk;
+    static sdk: typeof sdk = (window as any).rammerSdk;
     appConfig: any;
 
-    constructor(appConfig: any) {
-        this.sdk.init({
+    static init(appConfig: any) {
+        console.log('appConfig', appConfig);
+        SymblWebEngine.sdk.init({
             appId: appConfig.appId,
             appSecret: appConfig.appSecret,
-            basePath: appConfig.basePath ? appConfig.basePath : 'https://api.symbl.ai',
-            logLevel: 'debug'
-        });
-
+            basePath: appConfig.basePath || 'https://api.symbl.ai',
+            // logLevel: 'debug'
+        });  
     }
 
     startRealtimeRequest(realtimeConfig: any) {
-        this.sdk.startRealtimeRequest(realtimeConfig);
+        // this.sdk.startRealtimeRequest(realtimeConfig);
     }
 }
