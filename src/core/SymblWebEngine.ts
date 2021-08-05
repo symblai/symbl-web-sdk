6218211252cf2057afb9a077318060d23845fc2f
stokes-christopher <christopher.stokes@symbl.ai>
2021-08-05 14:16:17 -0500

    Feat: add basic Subscribe API functionality

--- src/core/SymblWebEngine.ts
+++ src/core/SymblWebEngine.ts
@@ -30,7 +30,7 @@ export = class SymblWebEngine {
      * @param {object} config - Symbl realtime request config object
      * @param {boolean} connect - indicate whether connection is immediate
      */
-    async startRealtimeRequest(config, connect) {
+    async startRealtimeRequest(config: any, connect: any) {
         const connection = await this.sdk.startRealtimeRequest(config);
         if (connect) {
             this.connect(connection);
@@ -42,7 +42,17 @@ export = class SymblWebEngine {
      * Manually connects to the Symbl WebSocket endpoint
      * @param {object} connection - Symbl realtime WebSocket connection object
      */
-    async connect(connection) {
+    async connect(connection: any) {
         await this.deviceManager.deviceConnect(connection);
+    }
+
+    /**
+     * Subscribe to existing connection in mostly non-interactive way
+     * @param {string} connectionId - connection ID created on connection init
+     * @param {function} cb - callback function to use data returned
+     */
+    async subscribe(connectionId: string, cb: any) {
+        const subscription = await this.sdk.subscribeToConnection(connectionId, cb);
+        return subscription;
     }
 }
