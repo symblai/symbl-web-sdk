10f6d2b2dd86c2e8d2b7943a60033d0b1397b33b
stokes-christopher <christopher.stokes@symbl.ai>
2021-08-10 18:39:49 -0500

    Style: update file to conform with eslint

--- src/core/SymblWebEngine.ts
+++ src/core/SymblWebEngine.ts
@@ -1,36 +1,87 @@
-const sdk = require('../../scripts/client.sdk.min.js');
-const DeviceManager = require('../workers/DeviceManager');
-const { ConfigError, NullError } = require('./services/ErrorHandler');
+const sdk = require("../../scripts/client.sdk.min.js");
+const DeviceManager = require("../workers/DeviceManager");
+const {ConfigError, NullError} = require("./services/ErrorHandler");
 
+interface SymblAppConfig {
+    appId: string;
+    appSecret: string;
+    basePath?: string;
+    logLevel?: string;
+}
+
+interface SymblRealtimeConnection {
+    conversationId: string;
+    sendAudio: unknown;
+    stop: unknown;
+}
+
+interface SymblRealtimeConfig {
+    id: string;
+    insightTypes?: Array<string>;
+    config?: {
+        meetingTitle?: string;
+        confidenceThreshold?: number;
+        timezoneOffset?: number;
+        languageCode?: string;
+        sampleRateHertz?: number;
+    };
+    speaker?: {
+        userId?: string;
+        name?: string;
+    }
+    handlers?: {
+        onSpeechDetected?: unknown;
+        onMessageResponse?: unknown;
+        onInsightResponse?: unknown;
+        onTopicResponse?: unknown;
+    }
+}
 
 export = class SymblWebEngine {
+
     sdk: typeof sdk = (window as any).rammerSdk;
-    appConfig: any;
+
+    appConfig: unknown;
+
     deviceManager: typeof DeviceManager;
 
-    constructor() {
+    constructor () {
+
         this.deviceManager = new DeviceManager();
+
     }
 
     /**
      * Initializes SymblWebEngine with application configuration
-     * @param {object} appConfig - Symbl configuration object  
+     * @param {object} appConfig - Symbl configuration object
      */
-    async init(appConfig: any) {
-        if (appConfig===null) { throw new NullError("AppConfig is null"); }
-        if (!appConfig.appId) { throw new ConfigError("AppID is missing"); }
-        if (!appConfig.appSecret) { throw new ConfigError("AppSecret is missing"); }
+    async init (appConfig: SymblAppConfig): Promise<object> {
+
+        if (appConfig === null) {
+
+            throw new NullError("AppConfig is null");
+
+        }
+        if (!appConfig.appId) {
+
+            throw new ConfigError("AppID is missing");
+
+        }
+        if (!appConfig.appSecret) {
+
+            throw new ConfigError("AppSecret is missing");
+
+        }
 
         await this.sdk.init({
-            appId: appConfig.appId,
-            appSecret: appConfig.appSecret,
-            basePath: appConfig.basePath || 'https://api.symbl.ai',
-            logLevel: 'debug'
+            "appId": appConfig.appId,
+            "appSecret": appConfig.appSecret,
+            "basePath": appConfig.basePath || "https://api.symbl.ai",
+            "logLevel": "debug"
         });
 
-        console.log("Connected to Symbl")
-        
         return this;
+
     }
 
     /**
@@ -38,27 +89,54 @@ export = class SymblWebEngine {
      * @param {object} config - Symbl realtime request config object
      * @param {boolean} connect - indicate whether connection is immediate
      */
-    async startRealtimeRequest(config: any, connect: boolean) {
-        if (config === null) { throw new NullError("Realtime config is null"); }
-        if (!config) { throw new ConfigError("Realtime config is missing"); }
-        if (!config.id) { throw new ConfigError("Meeting ID is missing"); }
+    async startRealtimeRequest (config: SymblRealtimeConfig, connect: boolean):
+        Promise<SymblRealtimeConnection> {
+
+        if (config === null) {
+
+            throw new NullError("Realtime config is null");
+
+        }
+        if (!config) {
+
+            throw new ConfigError("Realtime config is missing");
+
+        }
+        if (!config.id) {
+
+            throw new ConfigError("Meeting ID is missing");
+
+        }
 
         const connection = await this.sdk.startRealtimeRequest(config);
         if (connect) {
+
             this.connect(connection);
+
         }
         return connection;
+
     }
 
     /**
      * Manually connects to the Symbl WebSocket endpoint
      * @param {object} connection - Symbl realtime WebSocket connection object
      */
-    async connect(connection: any) {
-        if (connection === null) { throw new NullError("Realtime config is null"); }
-        if (!connection) { throw new ConfigError("Realtime websocket connection is missing"); }
+    async connect (connection: SymblRealtimeConnection): Promise<void> {
+
+        if (connection === null) {
+
+            throw new NullError("Realtime config is null");
+
+        }
+        if (!connection) {
+
+            throw new ConfigError("Realtime websocket connection is missing");
+
+        }
 
         await this.deviceManager.deviceConnect(connection);
+
     }
 
     /**
@@ -66,12 +144,28 @@ export = class SymblWebEngine {
      * @param {string} connectionId - connection ID created on connection init
      * @param {function} cb - callback function to use data returned
      */
-    async subscribeToStreaming(connectionId: string, cb: any) {
-        if (connectionId === null) { throw new NullError("Connection ID is null"); }
-        if (!connectionId) { throw new ConfigError("Connection ID is missing"); }
+    async subscribeToStreaming (connectionId: string, cb: unknown):
+        Promise<object> {
 
-        const subscription = await this.sdk.subscribeToConnection(connectionId, cb, true);
+        if (connectionId === null) {
+
+            throw new NullError("Connection ID is null");
+
+        }
+        if (!connectionId) {
+
+            throw new ConfigError("Connection ID is missing");
+
+        }
+
+        const subscription = await this.sdk.subscribeToConnection(
+            connectionId,
+            cb,
+            true
+        );
+
         return subscription;
+
     }
 
     /**
@@ -79,11 +173,28 @@ export = class SymblWebEngine {
      * @param {string} connectionId - connection ID created on connection init
      * @param {function} cb - callback function to use data returned
      */
-    async subscribeToTelephony(connectionId: string, cb: any) {
-        if (connectionId === null) { throw new NullError("Connection ID is null"); }
-        if (!connectionId) { throw new ConfigError("Connection ID is missing"); }
+    async subscribeToTelephony (connectionId: string, cb: unknown):
+        Promise<object> {
 
-        const subscription = await this.sdk.subscribeToConnection(connectionId, cb, false);
+        if (connectionId === null) {
+
+            throw new NullError("Connection ID is null");
+
+        }
+        if (!connectionId) {
+
+            throw new ConfigError("Connection ID is missing");
+
+        }
+
+        const subscription = await this.sdk.subscribeToConnection(
+            connectionId,
+            cb,
+            false
+        );
+
         return subscription;
+
     }
+
 }
