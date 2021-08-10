7a3c81e3786b115b368f641c096210c5aa02d05c
stokes-christopher <christopher.stokes@symbl.ai>
2021-08-10 17:07:36 -0500

    Fix: update ErrorHandler file to export correctly

--- src/core/services/ErrorHandler.ts
+++ src/core/services/ErrorHandler.ts
@@ -1,33 +1,31 @@
-export namespace ErrorHandler {
-	class ConnectionError extends Error {
-		constructor(message) {
-			super(message);
-			this.name = "ConnectionError";
-		}
-
+export class ConnectionError extends Error {
+	constructor(message) {
+		super(message);
+		this.name = "ConnectionError";
 	}
 
-	class HttpError extends Error {
-		constructor(message) {
-			super(message);
-			this.name = "HttpError";
-		}
+}
 
+export class HttpError extends Error {
+	constructor(message) {
+		super(message);
+		this.name = "HttpError";
 	}
 
-	class NullError extends Error {
-		constructor(message) {
-			super(message);
-			this.name = "NullError";
-		}
+}
 
+export class NullError extends Error {
+	constructor(message) {
+		super(message);
+		this.name = "NullError";
 	}
 
-	class ConfigError extends Error {
-		constructor(message) {
-			super(message);
-			this.name = "ConfigError";
-		}
+}
 
+export class ConfigError extends Error {
+	constructor(message) {
+		super(message);
+		this.name = "ConfigError";
 	}
+
 }
