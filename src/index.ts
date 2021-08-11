3bc786bc616e1c755cbaf6b76b3a1e13b5e28961
stokes-christopher <christopher.stokes@symbl.ai>
2021-08-10 19:01:46 -0500

    Style: update index to conform to eslint

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-/// <reference path="./types/base.ts" />
-/// <reference path="./types/conversation.ts" />
-/// <reference path="./types/responses.ts" />
-/// <reference path="./types/realtime.ts" />
+// / <reference path="./types/base.ts" />
+// / <reference path="./types/conversation.ts" />
+// / <reference path="./types/responses.ts" />
+// / <reference path="./types/realtime.ts" />
 
-const SymblWebEngine = require('./core/SymblWebEngine.js');
+const SymblWebEngine = require("./core/SymblWebEngine.js");
 
 export = SymblWebEngine;
