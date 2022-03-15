c4609af20c71c2d94c8e815e46ac610ecc93d04e
Christopher Stokes <christopher.stokes@symbl.ai>
2022-03-15 18:34:10 -0500

    Feature: Add network connectivity check

--- src2/network/NetworkConnectivityDetector.ts
+++ src2/network/NetworkConnectivityDetector.ts
@@ -1,56 +1,102 @@
-// import {sdk} from "@symblai/symbl-js/build/client.sdk.min";
-// import {NetworkEvent} from "../events";
-// import Logger from "../logger";
+import Logger from "../logger";
+import {NetworkEvent} from "../events";
+import {sdk} from "@symblai/symbl-js/build/client.sdk.min";
 
-// export class NetworkConnectivityDetector extends EventTarget {
-//     private sdk: sdk;
-//     private maxRetries: number;
-//     private checkInterval: number;
-//     private connectivityCheckIntervalRef: number;
-//     private logger: Logger = new Logger();
-    
-//     constructor(sdk: sdk) { // Instance of JS-SDK
-//         super();
-//         this.sdk = sdk
-//         this.sdk.setNetworkConnectivityDispatcher(this);
-//         // Add function bindings here
-//     }
-    
-//     public forceCheckNetworkConnectivity() {
-//         this.onlineDetector();
-//     }
-    
-//     private async onlineDetector() {
-//         this.maxRetries = 1200;
-//         this.checkInterval = 3000;
+export class NetworkConnectivityDetector extends EventTarget {
 
-//         if (this.connectivityCheckIntervalRef) {
-//             clearInterval(this.connectivityCheckIntervalRef);
-//         }
-    
-//         const connectivityCheckIntervalRef = setInterval(async () => {
-//             if (this.maxRetries > 0) {
-//                 try {
-//                     const response = await fetch('https://symbl-sdk-cdn-bucket.storage.googleapis.com');
-//                     if (response.ok) {
-//                         this.dispatchEvent(new NetworkEvent('offline', false));
-//                         if (this.connectivityCheckIntervalRef)
-//                             clearInterval(this.connectivityCheckIntervalRef);
-    
-//                         this.logger.debug(`Connection online!`);
-//                     } else {
-//                         this.dispatchEvent(new NetworkEvent('offline', true));
-//                         this.maxRetries -= 1;
-//                     }
-//                 } catch (e) {
-//                     this.dispatchEvent(new NetworkEvent('offline', true));
-//                     this.maxRetries -= 1;
-//                 }
-//             } else {
-//                 this.logger.warn(`Max retries to check for active internet connection exceeded! Please refresh the page when the internet is back online.`);
-//                 if (connectivityCheckIntervalRef)
-//                     clearInterval(connectivityCheckIntervalRef);
-//             }
-//         }, this.checkInterval);
-//     }
-// }
+    private sdk: sdk;
+
+    private maxRetries: number;
+
+    private checkInterval: number;
+
+    private connectivityCheckIntervalRef: number;
+
+    private logger: Logger = new Logger();
+
+    constructor (sdk: sdk) { // Instance of JS-SDK
+
+        super();
+        this.sdk = sdk;
+        this.sdk.setNetworkConnectivityDispatcher(this);
+        // Add function bindings here
+
+    }
+
+    public forceCheckNetworkConnectivity (): void {
+
+        this.onlineDetector();
+
+    }
+
+    private async onlineDetector (): Promise<void> {
+
+        this.maxRetries = 1200;
+        this.checkInterval = 3000;
+
+        if (this.connectivityCheckIntervalRef) {
+
+            clearInterval(this.connectivityCheckIntervalRef);
+
+        }
+
+        const connectivityCheckIntervalRef = setInterval(
+            async () => {
+
+                if (this.maxRetries > 0) {
+
+                    try {
+
+                        const response = await fetch("https://symbl-sdk-cdn-bucket.storage.googleapis.com");
+                        if (response.ok) {
+
+                            this.dispatchEvent(new NetworkEvent(
+                                "offline",
+                                false
+                            ));
+                            if (this.connectivityCheckIntervalRef) {
+
+                                clearInterval(this.connectivityCheckIntervalRef);
+
+                            }
+
+                            this.logger.debug("Connection online!");
+
+                        } else {
+
+                            this.dispatchEvent(new NetworkEvent(
+                                "offline",
+                                true
+                            ));
+                            this.maxRetries -= 1;
+
+                        }
+
+                    } catch (err) {
+
+                        this.dispatchEvent(new NetworkEvent(
+                            "offline",
+                            true
+                        ));
+                        this.maxRetries -= 1;
+
+                    }
+
+                } else {
+
+                    this.logger.warn("Max retries to check for active internet connection exceeded! Please refresh the page when the internet is back online.");
+                    if (connectivityCheckIntervalRef) {
+
+                        clearInterval(connectivityCheckIntervalRef);
+
+                    }
+
+                }
+
+            },
+            this.checkInterval
+        );
+
+    }
+
+}
