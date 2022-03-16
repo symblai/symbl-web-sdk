b60f96e6e73ac7dbb82cdc57f09892ad486ecadd
Christopher Stokes <christopher.stokes@symbl.ai>
2022-03-16 17:43:27 -0500

    Update: Add basic tests for connID regex

--- tests/symbl/uniquenessRegex.test.ts
+++ tests/symbl/uniquenessRegex.test.ts
@@ -1,24 +1,39 @@
-import { yourConstant } from "../../src/constants";
+import { PASSWORD_REGEX } from "../../src/constants";
+import {v4 } from "uuid";
+export const uuid = v4;
 
 test(
     "Should pass",
     async () => {
         const regex = new RegExp(
-            yourConstant,
+            PASSWORD_REGEX,
             "u"
         );
-        const validSessionId = regex.test("abc123");
+        const validSessionId = regex.test("abc123-");
 
  		expect(validSessionId).toBeTruthy();
 	}
 );
+
+test(
+    "UUID test",
+    async () => {
+        const regex = new RegExp(
+            PASSWORD_REGEX,
+            "u"
+        );
+        const validSessionId = regex.test(uuid());
+
+        expect (validSessionId).toBeTruthy();
+    }
+)
 
 
 test(
     "Should fail",
     async () => {
         const regex = new RegExp(
-            yourConstant,
+            PASSWORD_REGEX,
             "u"
         );
         const validSessionId = regex.test("abc");
