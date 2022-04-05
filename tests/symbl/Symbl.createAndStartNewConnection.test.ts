197c6c83462a07fd708ceb83413dcf36781da1f9
Christopher Stokes <christopher.stokes@symbl.ai>
2022-04-05 17:44:04 -0500

    Update: linting tests

--- tests/symbl/Symbl.createAndStartNewConnection.test.ts
+++ tests/symbl/Symbl.createAndStartNewConnection.test.ts
@@ -1,64 +1,88 @@
+import {APP_ID, APP_SECRET} from "../constants";
+import {LINEAR16AudioStream, OpusAudioStream} from "../../src/audio";
+import {ConnectionFactory} from "../../src/connection";
+import {ConnectionState} from "../../src/types";
+import {StreamingAPIConnection} from "../../src/api";
 import Symbl from "../../src/symbl";
-import { ConnectionFactory, BaseConnection } from '../../src/connection';
-import { StreamingAPIConnection  } from "../../src/api";
-import { Recorder } from "symbl-opus-encdec";
-import {sdk} from "@symblai/symbl-js/build/client.sdk.min";
-import { ConnectionProcessingState, ConnectionState } from "../../src/types";
-import { LINEAR16AudioStream, OpusAudioStream } from '../../src/audio';
-import { APP_ID, APP_SECRET } from '../constants';
 
-/** define mocks */
+/** Define mocks */
 jest.mock("symbl-opus-encdec");
 const startProcessingMock = jest.fn(() => {
     // (StreamingAPIConnection as any).processingState = ConnectionProcessingState.PROCESSING;
-})
-jest.mock("../../src/api", () => {
-    return {
-        StreamingAPIConnection: jest.fn().mockImplementation(() => {
-            return {
-                connect: jest.fn(() => {
-                    (StreamingAPIConnection as any).connectionState = ConnectionState.CONNECTED;
-                }),
-                startProcessing: startProcessingMock,
-                sdk: {
-                    createStream: jest.fn()
-                },
-                stream: {
-                    start: jest.fn()
-                }
-            }
-        })
-    }
 });
-jest.mock('../../src/connection', () => {
-    return {
-        ConnectionFactory: jest.fn().mockImplementation(() => {
-            return {
-                instantiateConnection: jest.fn(() => {
-                    return new StreamingAPIConnection({} as any, {} as any) as any;
-                })
-            }
-        })
+jest.mock(
+    "../../src/api",
+    () => {
+
+        return {
+            "StreamingAPIConnection": jest.fn().mockImplementation(() => {
+
+                return {
+                    "connect": jest.fn(() => {
+
+                        (StreamingAPIConnection as any).connectionState = ConnectionState.CONNECTED;
+
+                    }),
+                    "sdk": {
+                        "createStream": jest.fn()
+                    },
+                    "startProcessing": startProcessingMock,
+                    "stream": {
+                        "start": jest.fn()
+                    }
+                };
+
+            })
+        };
+
     }
-})
-/** end mocks definition **/
+);
+jest.mock(
+    "../../src/connection",
+    () => {
+
+        return {
+            "ConnectionFactory": jest.fn().mockImplementation(() => {
+
+                return {
+                    "instantiateConnection": jest.fn(() => {
+
+                        return new StreamingAPIConnection(
+{} as any,
+{} as any
+                        ) as any;
+
+                    })
+                };
+
+            })
+        };
+
+    }
+);
+
+/** End mocks definition **/
 
 
-let pcmStream, opusStream;
+let opusStream, pcmStream;
 beforeAll(() => {
+
     const opusConfig: any = {
-        numberOfChannels: 1,
-        encoderSampleRate: 48000,
-        encoderFrameSize: 20,
-        maxFramesPerPage: 40,
-        encoderComplexity: 6,
-        streamPages: true,
-        rawOpus: true
+        "encoderComplexity": 6,
+        "encoderFrameSize": 20,
+        "encoderSampleRate": 48000,
+        "maxFramesPerPage": 40,
+        "numberOfChannels": 1,
+        "rawOpus": true,
+        "streamPages": true
     };
     const context = new AudioContext();
     const mediaStream = new MediaStream();
     const sourceNode = context.createMediaStreamSource(mediaStream);
-    opusStream = new OpusAudioStream(sourceNode, opusConfig);
+    opusStream = new OpusAudioStream(
+        sourceNode,
+        opusConfig
+    );
     pcmStream = new LINEAR16AudioStream(sourceNode);
 
 });
@@ -66,125 +90,157 @@ beforeAll(() => {
 test(
     "Symbl.createAndStartNewConnection - Calling createAndStartNewConnection with valid config without passing AudioStream",
     async () => {
+
         const authConfig = {
-            appId: APP_ID,
-            appSecret: APP_SECRET
+            "appId": APP_ID,
+            "appSecret": APP_SECRET
         };
 
         const symbl = new Symbl(authConfig);
         const connectionConfig = {
-            id: "209u3ij",
-            insightTypes: ['action_item', 'question'],
-            config: {
-                meetingTitle: 'My Test Meeting',
-                confidenceThreshold: 0.7,
-                timezoneOffset: 480,
-                languageCode: 'en-US',
+            "config": {
+                "confidenceThreshold": 0.7,
+                "languageCode": "en-US",
+                "meetingTitle": "My Test Meeting",
+                "timezoneOffset": 480
             },
-            speaker: {
-                userId: 'emailAddress',
-                name: 'My name'
-            },
-        };            
+            "id": "209u3ij",
+            "insightTypes": [
+                "action_item",
+                "question"
+            ],
+            "speaker": {
+                "name": "My name",
+                "userId": "emailAddress"
+            }
+        };
 
-        const connectSpy = jest.spyOn(symbl, 'createConnection');
+        const connectSpy = jest.spyOn(
+            symbl,
+            "createConnection"
+        );
         const connection: any = await symbl.createAndStartNewConnection(connectionConfig);
         expect(connectSpy).toHaveBeenCalledTimes(1);
         expect(ConnectionFactory).toHaveBeenCalledTimes(1);
         expect(connection instanceof StreamingAPIConnection);
         expect(startProcessingMock).toBeCalledTimes(1);
 
-        // expect(connection.processingState).toBe(ConnectionProcessingState.PROCESSING);
+        // Expect(connection.processingState).toBe(ConnectionProcessingState.PROCESSING);
+
     }
 );
 
 test(
     "Symbl.createAndStartNewConnection - Calling createAndStartNewConnection with valid config that contains an id",
     async () => {
+
         const authConfig = {
-            appId: APP_ID,
-            appSecret: APP_SECRET
+            "appId": APP_ID,
+            "appSecret": APP_SECRET
         };
         const id = "asidjf23sdf";
         const symbl = new Symbl(authConfig);
         const connectionConfig = {
+            "config": {
+                "confidenceThreshold": 0.7,
+                "languageCode": "en-US",
+                "meetingTitle": "My Test Meeting",
+                "timezoneOffset": 480
+            },
             id,
-            insightTypes: ['action_item', 'question'],
-            config: {
-                meetingTitle: 'My Test Meeting',
-                confidenceThreshold: 0.7,
-                timezoneOffset: 480,
-                languageCode: 'en-US',
-            },
-            speaker: {
-                userId: 'emailAddress',
-                name: 'My name'
-            },
+            "insightTypes": [
+                "action_item",
+                "question"
+            ],
+            "speaker": {
+                "name": "My name",
+                "userId": "emailAddress"
+            }
         };
-        
-        const connectSpy = jest.spyOn(symbl, 'createConnection');
+
+        const connectSpy = jest.spyOn(
+            symbl,
+            "createConnection"
+        );
         const connection: any = await symbl.createAndStartNewConnection(connectionConfig);
         expect(connectSpy).toHaveBeenCalledTimes(1);
         expect(connection instanceof StreamingAPIConnection);
         expect(startProcessingMock).toBeCalledTimes(1);
+
     }
 );
 
 test(
     "Symbl.createAndStartNewConnection - Calling createAndStartNewConnection with valid config and passing in LINEAR16AudioStream",
     async () => {
+
         const authConfig = {
-            appId: APP_ID,
-            appSecret: APP_SECRET
+            "appId": APP_ID,
+            "appSecret": APP_SECRET
         };
         const symbl = new Symbl(authConfig);
         const connectionConfig = {
-            id: "209u3ij",
-            insightTypes: ['action_item', 'question'],
-            config: {
-                meetingTitle: 'My Test Meeting',
-                confidenceThreshold: 0.7,
-                timezoneOffset: 480,
-                languageCode: 'en-US',
+            "config": {
+                "confidenceThreshold": 0.7,
+                "languageCode": "en-US",
+                "meetingTitle": "My Test Meeting",
+                "timezoneOffset": 480
             },
-            speaker: {
-                userId: 'emailAddress',
-                name: 'My name'
-            },
+            "id": "209u3ij",
+            "insightTypes": [
+                "action_item",
+                "question"
+            ],
+            "speaker": {
+                "name": "My name",
+                "userId": "emailAddress"
+            }
         };
-        const connection: any = await symbl.createAndStartNewConnection(connectionConfig, pcmStream);
+        const connection: any = await symbl.createAndStartNewConnection(
+            connectionConfig,
+            pcmStream
+        );
         expect(ConnectionFactory).toHaveBeenCalledTimes(1);
         expect(connection instanceof StreamingAPIConnection);
         expect(startProcessingMock).toBeCalledTimes(1);
+
     }
 );
 
 test(
     "Symbl.createAndStartNewConnection - Calling createAndStartNewConnection with valid config and passing in OpusAudioStream",
     async () => {
+
         const authConfig = {
-            appId: APP_ID,
-            appSecret: APP_SECRET
+            "appId": APP_ID,
+            "appSecret": APP_SECRET
         };
         const symbl = new Symbl(authConfig);
         const connectionConfig = {
-            id: "209u3ij",
-            insightTypes: ['action_item', 'question'],
-            config: {
-                meetingTitle: 'My Test Meeting',
-                confidenceThreshold: 0.7,
-                timezoneOffset: 480,
-                languageCode: 'en-US',
+            "config": {
+                "confidenceThreshold": 0.7,
+                "languageCode": "en-US",
+                "meetingTitle": "My Test Meeting",
+                "timezoneOffset": 480
             },
-            speaker: {
-                userId: 'emailAddress',
-                name: 'My name'
-            },
+            "id": "209u3ij",
+            "insightTypes": [
+                "action_item",
+                "question"
+            ],
+            "speaker": {
+                "name": "My name",
+                "userId": "emailAddress"
+            }
         };
-        const connection: any = await symbl.createAndStartNewConnection(connectionConfig, opusStream);
+        const connection: any = await symbl.createAndStartNewConnection(
+            connectionConfig,
+            opusStream
+        );
         expect(ConnectionFactory).toHaveBeenCalledTimes(1);
         expect(connection instanceof StreamingAPIConnection);
         expect(startProcessingMock).toBeCalledTimes(1);
+
     }
 );
 
