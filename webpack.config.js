7d021c2c3b48a66fbb57bff57403769b8573008c
avoliva <adam.voliva@symbl.ai>
2022-04-27 17:18:52 -0700

    Fix window export.

--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: '[name].js',
+        libraryTarget: 'global',
     },
      performance: {
         hints: false,
